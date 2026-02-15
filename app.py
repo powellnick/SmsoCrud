856cccccc817404790717d37ed432436723cd957
powellnick <npowell.seattle@gmail.com>
2026-02-14 20:37:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,8 @@ def using_firestore() -> bool:
     try:
         if st.session_state.get("_force_sqlite"):
             return False
+        if bool(st.secrets.get("force_sqlite", False)):
+            return False
         return ("firebase_service_account" in st.secrets) or ("gcp_service_account" in st.secrets)
     except Exception:
         return False
